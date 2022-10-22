8b953d87140583c0bdb2019f50a165f2ec1c7683
Cole Wyant <cwyant314159@gmail.com>
2022-10-22 12:57:36 -0500

    Change scrolling shortcut to de-conflict with nvim keybindings.

--- config.h
+++ config.h
@@ -186,7 +186,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY      Mod1Mask
+#define MODKEY      Mod4Mask
 #define SHIFTMOD    (ShiftMask|MODKEY)
 #define TERMMOD     (ControlMask|ShiftMask)
 
